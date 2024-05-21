c6a405e4967092d6839413c8e9d90d46c2e8f784
Dmytro Andreikiv <info@dma-mobile.dev>
2024-05-21 16:55:14 +0200

    Add TraceInfo struct

--- Sources/TraceInfo/TraceInfo.swift
+++ Sources/TraceInfo/TraceInfo.swift
@@ -1,2 +1,19 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+struct TraceInfo: Codable {
+    /// Channel/queue `index` to which `count` and `sequence` belong.
+    ///
+    /// A number between 0 and up to `maxConcurrent` parameter
+    /// received during enrollment process.
+    let queueIndex: UInt
+
+    /// TOTAL number of requests SENT.
+    var sent: UInt
+
+    /// Number of SUCCEEDED requests (SUCCESSFUL responses RECEIVED).
+    var succeeded: UInt
+
+    init(queueIndex: UInt, sent: UInt = 0, succeeded: UInt = 0) {
+        self.queueIndex = queueIndex
+        self.sent = sent
+        self.succeeded = succeeded
+    }
+}
