3753ac9e65c9dffaafa84f033f961ba69bb583c9
Dmytro Andreikiv <info@dma-mobile.dev>
2024-05-22 05:35:59 +0200

    Add mutating functions

--- Sources/TraceInfo/TraceInfo.swift
+++ Sources/TraceInfo/TraceInfo.swift
@@ -1,19 +1,29 @@
-struct TraceInfo: Codable {
+public struct TraceInfo: Codable {
     /// Channel/queue `index` to which `count` and `sequence` belong.
     ///
     /// A number between 0 and up to `maxConcurrent` parameter
     /// received during enrollment process.
-    let queueIndex: UInt
+    public let queueIndex: UInt
 
     /// TOTAL number of requests SENT.
-    var sent: UInt
+    public private(set) var sent: UInt
 
     /// Number of SUCCEEDED requests (SUCCESSFUL responses RECEIVED).
-    var succeeded: UInt
+    public private(set) var succeeded: UInt
 
     init(queueIndex: UInt, sent: UInt = 0, succeeded: UInt = 0) {
         self.queueIndex = queueIndex
         self.sent = sent
         self.succeeded = succeeded
+    }
+}
+
+extension TraceInfo {
+    public mutating func incrementSent() {
+        sent += 1
+    }
+
+    public mutating func incrementSucceeded() {
+        sent += 1
     }
 }
